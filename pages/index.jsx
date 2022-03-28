213fc695dd3fcfaa1ddb2d9537c5a70a757cf1d5
oriel <orianafarrugia3@gmail.com>
2022-03-27 22:35:21 -0500

    stuff

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,17 +3,21 @@ import Link from "next/link"
 
 export default function Home() {
   return <div>
-    <nav className="flex header">
-      <input className="" type="text" placeholder="type..." />
+    <div id="body" className="center-container">
+      <div id="" className="cli">
+        <input className="" type="text" placeholder="type..." />
+      </div>
 
+    <nav className="flex header">
       <button className="mobile-header-menu-toggle" aria-controls="header-menu" aria-expanded="false"><span className="sr-only">Menu</span></button>
 
       <ul id="header-menu" className="flex header-menu" data-visible="false">
-        <li><Link href="/"><a><span aria-hidden="true">00</span>Home</a></Link></li>
-        <li><Link href="/tech"><a><span aria-hidden="true">01</span>Tech</a></Link></li>
-        <li><Link href="/food"><a><span aria-hidden="true">02</span>Food</a></Link></li>
-        <li><Link href="/fiction"><a><span aria-hidden="true">03</span>Fiction</a></Link></li>
+        <li><Link href="/"><a>Home</a></Link></li>
+        <li><Link href="/tech"><a>Tech</a></Link></li>
+        <li><Link href="/food"><a>Food</a></Link></li>
+        <li><Link href="/fiction"><a>Fiction</a></Link></li>
       </ul>
     </nav>
+    </div>
   </div>
 }
